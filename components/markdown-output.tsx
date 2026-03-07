09c2318cbc04d2ee1dbeb0ce83722873f915931f
jerrykankelborg <jkankelborg@csumb.edu>
2026-03-06 18:26:58 -0800

    add tables and lists to output

--- components/markdown-output.tsx
+++ components/markdown-output.tsx
@@ -1,14 +1,17 @@
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+import remarkGfm from "remark-gfm";
 
 export default function MarkdownOutput({ content }: { content: string }) {
   return (
     <ReactMarkdown
+      remarkPlugins={[remarkGfm]}
       components={{
         code({ className, children, node, ...props }) {
           const match = /language-(\w+)/.exec(className || "");
-          const isInline = node?.position?.start.line === node?.position?.end.line && !match;
+          const isInline =
+            node?.position?.start.line === node?.position?.end.line && !match;
 
           if (!isInline) {
             return (
